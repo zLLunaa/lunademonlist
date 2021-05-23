b1cd98cde782e7b57d2bc418777d6670d41e2266
zLLunaa <84684775+zLLunaa@users.noreply.github.com>
2021-05-23 18:43:49 -0400

    Update list.js

--- js/list.js
+++ js/list.js
@@ -295,7 +295,483 @@ const d = {
                        "key": 20
         },
         /*=================================================================================*/
-	
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Dimension Breaker",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 55539300,
+                       "pass": "",
+                       "percentToQualify": 56,
+                       "verificationVid": "https://youtu.be/3z4NkqOS4KA",
+                       "key": 21
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Phosphorescent",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 56846226,
+                       "pass": "",
+                       "percentToQualify": 52,
+                       "verificationVid": "https://youtu.be/eF83vXVgsE0",
+                       "key": 22
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Blade of Justice",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 35448603,
+                       "pass": "",
+                       "percentToQualify": 64,
+                       "verificationVid": "https://www.youtube.com/watch?v=MbJbIP1fYME",
+                       "key": 23
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Artifice",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 57520174,
+                       "pass": "",
+                       "percentToQualify": 52,
+                       "verificationVid": "https://youtu.be/ig-z4F2rbZY",
+                       "key": 24
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Biohazard",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 61997741,
+                       "pass": "",
+                       "percentToQualify": 53,
+                       "verificationVid": "https://youtu.be/Rerl-N1pvuo",
+                       "key": 25
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Atmosphere",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 42664144,
+                       "pass": "",
+                       "percentToQualify": 56,
+                       "verificationVid": "https://youtu.be/j6U1whaVETM",
+                       "key": 26
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Bloodbath",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 10565740,
+                       "pass": "",
+                       "percentToQualify": 54,
+                       "verificationVid": "https://youtu.be/KI72VgVOqrE",
+                       "key": 27
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Down Bass",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 8147005,
+                       "pass": "",
+                       "percentToQualify": 58,
+                       "verificationVid": "https://youtu.be/uzIE4EQKTq8",
+                       "key": 28
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Azure Fiesta",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 60923124,
+                       "pass": "",
+                       "percentToQualify": 55,
+                       "verificationVid": "https://youtu.be/cc7qstGtgUw",
+                       "key": 29
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Esencia",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 54988894,
+                       "pass": "",
+                       "percentToQualify": 44,
+                       "verificationVid": "https://youtu.be/rhhOgZEo8CQ",
+                       "key": 30
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Novalis",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 33748382,
+                       "pass": "",
+                       "percentToQualify": 54,
+                       "verificationVid": "https://youtu.be/2DK0SiShI2M",
+                       "key": 31
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Aftermath",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 25610878,
+                       "pass": "",
+                       "percentToQualify": 57,
+                       "verificationVid": "https://youtu.be/024PCNnSYac",
+                       "key": 32
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Prismatic Haze",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 59899374,
+                       "pass": "",
+                       "percentToQualify": 61,
+                       "verificationVid": "https://youtu.be/EnbJaDPuNeM",
+                       "key": 33
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Breakout",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 25241513,
+                       "pass": "",
+                       "percentToQualify": 52,
+                       "verificationVid": "https://youtu.be/o0aNqhCkeg4",
+                       "key": 34
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Niflheim",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 30194711,
+                       "pass": "",
+                       "percentToQualify": 54,
+                       "verificationVid": "https://youtu.be/s-kPRNmxEUA",
+                       "key": 35
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Artificial Dream",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 38550757,
+                       "pass": "",
+                       "percentToQualify": 60,
+                       "verificationVid": "https://youtu.be/zb0nHNMr5DI",
+                       "key": 36
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "The Flawless",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 21280210,
+                       "pass": "",
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/FIeuPsX6LAY",
+                       "key": 37
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "CYCLONE",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 54917442,
+                       "pass": "",
+                       "percentToQualify": 55,
+                       "verificationVid": "https://youtu.be/8FZuE_GGDEg",
+                       "key": 38
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Annihilation Nation",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 48008831,
+                       "pass": "",
+                       "percentToQualify": 61,
+                       "verificationVid": "https://youtu.be/opcEi8gJUks",
+                       "key": 39
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "UltraSans",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 50008168,
+                       "pass": "",
+                       "percentToQualify": 56,
+                       "verificationVid": "https://youtu.be/lteINY5v1mU",
+                       "key": 40
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Deception Dive",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 34379100,
+                       "pass": "",
+                       "percentToQualify": 76,
+                       "verificationVid": "https://youtu.be/qiflxAslu6s",
+                       "key": 41
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Raindance",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 57019271,
+                       "pass": "",
+                       "percentToQualify": 59,
+                       "verificationVid": "https://youtu.be/EI82rUHo0qc",
+                       "key": 42
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Alcatraz",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 57208977,
+                       "pass": "",
+                       "percentToQualify": 63,
+                       "verificationVid": "https://youtu.be/SONKZHXc1jY",
+                       "key": 43
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Incipient",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 32688321,
+                       "pass": "",
+                       "percentToQualify": 60,
+                       "verificationVid": "https://youtu.be/ef-L_YsF4tQ",
+                       "key": 44
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Unearthed",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 50833396,
+                       "pass": "",
+                       "percentToQualify": 56,
+                       "verificationVid": "https://youtu.be/BNkJBp66SfA",
+                       "key": 45
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Penombre",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 48454650,
+                       "pass": "",
+                       "percentToQualify": 57,
+                       "verificationVid": "https://youtu.be/nwfpyNEI9TQ",
+                       "key": 46
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Allegiance",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 20761188,
+                       "pass": "",
+                       "percentToQualify": 61,
+                       "verificationVid": "https://youtu.be/vqxkZFJEQ3s",
+                       "key": 47
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Cataclysm",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 3979721,
+                       "pass": "",
+                       "percentToQualify": 52,
+                       "verificationVid": "https://youtu.be/fMGS5kBGmMw",
+                       "key": 48
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Torgue",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 60516506,
+                       "pass": "",
+                       "percentToQualify": 62,
+                       "verificationVid": "https://youtu.be/_B2cmDjlNlY",
+                       "key": 49
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "FinKor",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 59585590,
+                       "pass": "",
+                       "percentToQualify": 56,
+                       "verificationVid": "https://youtu.be/WVXfMoJmBwU",
+                       "key": 50
+        },
+        /*=================================================================================*/	
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Thanatophobia",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 62809982,
+                       "pass": "",
+                       "percentToQualify": 60,
+                       "verificationVid": "https://youtu.be/vW-H-cpFfqQ",
+                       "key": 51
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Ice Carbon Diablo X",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 814678,
+                       "pass": "",
+                       "percentToQualify": 64,
+                       "verificationVid": "https://youtu.be/n_OvZT59vh8",
+                       "key": 52
+        },
+        /*=================================================================================*/
+	{
+                       "vids": [
+              
+                       ],
+                       "name": "Gunslinga Corridor",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 58069223,
+                       "pass": "",
+                       "percentToQualify": 69,
+                       "verificationVid": "https://youtu.be/cOIIjsfzha8",
+                       "key": 53
+        },
+        /*=================================================================================*/
+			{
+                       "vids": [
+              
+                       ],
+                       "name": "Acu",
+                       "author": "luna",
+                       "more": "none",
+                       "id": 61079355,
+                       "pass": "",
+                       "percentToQualify": 63,
+                       "verificationVid": "https://youtu.be/sTlu8C1JCd0",
+                       "key": 54
+        },
+        /*=================================================================================*/
+		
 	],
 	"version": [
 		1,
