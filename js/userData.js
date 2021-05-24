9a6efc108b7ae7c273426b67f5cfc79e3de9356b
zLLunaa <84684775+zLLunaa@users.noreply.github.com>
2021-05-23 20:43:23 -0400

    Update userData.js

--- js/userData.js
+++ js/userData.js
@@ -51,7 +51,7 @@ function userList() {
                         if (user_data[b].highest == "null" && parseInt(entry2.percent) == 100) {
                             user_data[b].highest = entry.name; 
                         }
-                        user_data[b].progress.push({map : entry.name.toString(), progress : entry2.percent.toString(), link : entry2.link, score : roundNumber(p,3), rank : i+1, hz : (entry2.hz != null ? entry2.hz : "144hz")});
+                        user_data[b].progress.push({map : entry.name.toString(), progress : entry2.percent.toString(), link : entry2.link, score : roundNumber(p,3), rank : i+0, hz : (entry2.hz != null ? entry2.hz : "144hz")});
                     }
                 }
             } else {
@@ -61,7 +61,7 @@ function userList() {
                 }
 
                 var prog = [];
-                prog.push({map : entry.name.toString(), progress : entry2.percent.toString(), link : entry2.link, score : roundNumber(p,3), rank : i+1, hz : (entry2.hz != null ? entry2.hz : "144hz")});
+                prog.push({map : entry.name.toString(), progress : entry2.percent.toString(), link : entry2.link, score : roundNumber(p,3), rank : i+0, hz : (entry2.hz != null ? entry2.hz : "144hz")});
 
                 user_data.push({name : entry2.user, highest : map, progress : prog, point : p, verified : []});
             }
@@ -90,11 +90,11 @@ function getUserData(user) {
     for (var i = 0 ; i < user_data[user].verified.length ; i++) {
         rank = user_data[user].verified[i] - 1;
         clears++;
-        progresses = progresses + '<li>' + list[rank].name + ' Verified </strong>(#'+(rank+1)+' / UP: '+roundNumber(getUserPoint(rank+1, 100, list[rank].percentToQualify, "144hz")*1, 3)+')<strong></a></li>'
+        progresses = progresses + '<li>' + list[rank].name + ' Verified </strong>(#'+(rank+0)+' / UP: '+roundNumber(getUserPoint(rank+0, 100, list[rank].percentToQualify, "144hz")*1, 3)+')<strong></a></li>'
     }
 
     for (var i = 0 ; i < user_data[user].progress.length ; i++) {
-        progresses = progresses + '<li><a href="'+user_data[user].progress[i].link+'" target="blank_">' + user_data[user].progress[i].map + ' ' + user_data[user].progress[i].progress + '% </strong>(#'+user_data[user].progress[i].rank+' / UP: '+user_data[user].progress[i].score+''+(parseInt(user_data[user].progress[i].hz.replace("hz", "")) >= 500 ? '' : ' / '+user_data[user].progress[i].hz)+')<strong></a></li>'
+        progresses = progresses + '<li><a href="'+user_data[user].progress[i].link+'" target="blank_">' + user_data[user].progress[i].map + ' ' + user_data[user].progress[i].progress + '% </strong>(#'+user_data[user].progress[i].rank+' / UP: '+user_data[user].progress[i].score+''+(parseInt(user_data[user].progress[i].hz.replace("hz", "")) >= 120 ? '' : ' / '+user_data[user].progress[i].hz)+')<strong></a></li>'
         if (user_data[user].progress[i].progress == 100) {
             clears++;
         }
