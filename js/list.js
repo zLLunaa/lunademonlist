4a6c9c65cf350b741c31f887685a31a29639bde0
zLLunaa <84684775+zLLunaa@users.noreply.github.com>
2021-05-23 21:32:15 -0400

    Update list.js

--- js/list.js
+++ js/list.js
@@ -623,7 +623,7 @@ const d = {
 				       "user": "Vink",
 				       "link": "https://youtu.be/q9W_LYllxCc",
 				       "percent": 100,
-				       "hz": ""
+				       "hz": "60hz"
 			       },
                        ],
                        "name": "CYCLONE",
